f4cd504b0dde3a95c6849362afdc1de6cbbf95db
Ziterz <ziady.mubaraq@gmail.com>
2024-01-11 22:19:03 +0700

    learn: day 16 create project called WeSplit using SwiftUI

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ziady Mubaraq on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
